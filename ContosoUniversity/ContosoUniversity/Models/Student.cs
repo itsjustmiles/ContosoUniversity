6cfc843767c95cdc9e3d7f0e15ea5b3f0a9aed2e
itsjustmiles <miles.brownuhs@gmail.com>
2014-07-07 13:51:31 -0700

    Added Partially Completed Project
    Currently working on Logger.cs

---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set;} 
+        public String LastName {get; set;}
+        public string FirstMidName { get; set; }
+        public DateTime Enrollmentdate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        
+    }
+}
